c83bbb3f997e30b05e01b37377a6e8c0b37b5c6a
Jan Gleixner <jan.gleixner@gmail.com>
2015-10-14 18:16:14 +0200

    fixed LogLik computation

--- R/CAM.R
+++ R/CAM.R
@@ -368,10 +368,10 @@ residuals.cam <- function(object, ...) object$data - object$fitted.values
 logLik.cam <- function(object, ...) {
     res <- residuals(object)
     N <- nrow(res)
-    val <-  sum(-1/2*N*(log(sapply(res^2,mean))+log(2*pi)+1))
-    logLiks <- lapply(object$nodeModels, logLik)
+    val <-  sum(-1/2*N*(log(sapply(res*res,mean))+log(2*pi)+1))
+    #logLiks <- lapply(object$nodeModels, logLik)
     #val <- do.call(sum, logLiks)
-    attr(val, "df") <- sum(sapply(logLiks,function(obj)attr(obj, "df"))) #there are more, but let's ignore them
+    attr(val, "df") <- NA#sum(sapply(logLiks,function(obj)attr(obj, "df"))) #there are more, but let's ignore them
     class(val) <- "logLik"
     return(val)
 }
